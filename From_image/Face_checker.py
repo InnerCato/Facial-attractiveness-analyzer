997d4132dc5998520e74c3c18c11e7d6be70a7cc
InnerCato <81331886+InnerCato@users.noreply.github.com>
2025-10-07 16:10:26 +0300

    Fix

--- From_image/Face_checker.py
+++ From_image/Face_checker.py
@@ -7,7 +7,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 predictor_path = os.path.join(base_dir, "shape_predictor_68_face_landmarks.dat")
 num = int(input("Enter image num: "))
 
-image_dir = os.path.join(base_dir, "Images")
+image_dir = os.path.join(base_dir, "Image")
 image_path = None
 for ext in [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp"]:
     candidate = os.path.join(image_dir, f"face{num}{ext}")
